47446e2262f58b810d34626794cee53d65ccab71
Aphiwe Maphanga <maphangaaphiwe7@gmail.com>
2026-07-11 13:23:15 +0200

    fixied bug on calculating overlapping times

--- wage_calculator.cpp
+++ wage_calculator.cpp
@@ -3,12 +3,14 @@ using  namespace std;
 
 int main() {
     //variable declearations
-    const double AFTERNOON_RATE = 32.40;
+    const double AFTERNOON_RATE = 32.50;
     const int EVENING_RATE = 44;
     int starting_time, finishing_time;
     double wage;
 
     // get inputs from the user
+    cout << "WAGE CALCULATION" << endl;
+    cout << "====================" << endl;
     cout << "Enter your starting time: ";
     cin >> starting_time;
 
@@ -19,10 +21,10 @@ int main() {
     if(finishing_time <= 6){
         wage = (finishing_time - starting_time) * AFTERNOON_RATE;
     }
-    if(starting_time < 6 && finishing_time > 6){
+    else if (starting_time < 6 && finishing_time > 6){
         wage = (6 - starting_time) * AFTERNOON_RATE + (finishing_time - 6) * EVENING_RATE;
     }
-    if(finishing_time >= 6){
+    else if (finishing_time >= 6){
         wage = (finishing_time - starting_time) * EVENING_RATE;
     }
 
