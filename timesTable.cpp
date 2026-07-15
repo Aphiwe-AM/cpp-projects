8e187227711e63d8410c1fc5ba9229ab7935bb2c
Aphiwe Maphanga <maphangaaphiwe7@gmail.com>
2026-07-15 21:47:47 +0200

    completed timestable.cpp

--- timesTable.cpp
+++ timesTable.cpp
@@ -4,5 +4,16 @@ using namespace std;
 // Write a program that displays a times table for any number.
 
 int main(){
-    
+    //variables 
+    int number, answer;
+
+    //prompt the user for a number
+    cout << "Enter a number: ";
+    cin >> number;
+
+    //create a loop to eterate and create the times table
+    for(int i = 1; i <= 12; i++){
+        answer = number * i;
+        cout << number << "x" << i << " = " << answer << endl;
+    }
 }
