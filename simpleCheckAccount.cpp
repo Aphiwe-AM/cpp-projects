724417cb0ce5f485afd79d6ea77be33fee7bc4d0
Aphiwe Maphanga <maphangaaphiwe7@gmail.com>
2026-07-15 19:37:49 +0200

    completed the simple check account.

--- simpleCheckAccount.cpp
+++ simpleCheckAccount.cpp
@@ -3,17 +3,22 @@ using namespace std;
 /*Write a program that maintains a simple cheque account. It first asks for a balance and then for a sequence
 of transactions. Deposits are given as positive values and cheques written as negative values. After each
 transaction, the program should display the new balance. The transactions end when the value 0 is entered.*/
+
 int main(){
     //variables decleared
     double balance;
     double value;
-    //prompt the user to input 
-    cout << "Please input cheque(-) or deposit(+) value (to terminate the program enter 0): ";
-    
-    //set an while loop that will terminate when user inters 0
-    while(cin >> value != 0){
 
+    //set an while loop that will terminate when user inters 0
+    do{
+        cout << "Please input cheque(-) or deposit(+) value (to terminate the program enter 0): ";
+        cin >> value;
+        
         balance += value;
 
-    }
+    }while(value != 0);
+
+    cout << "The account balance is: " << balance;
+
+   
 }
--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(){ 
     int x;
+    
     cin >> x;
     while(x != 1){
         if(x % 2 == 1){
