06364e5f3bb41cd3fa3e0ebc6b7f942904a8b44e
Aphiwe Maphanga <maphangaaphiwe7@gmail.com>
2026-07-15 21:14:11 +0200

    completed compoundinterest.cpp

--- compoundInterest.cpp
+++ compoundInterest.cpp
@@ -5,3 +5,31 @@ using namespace std;
 number of years. After inputting an amount of Rands, an interest rate and a number of years to invest the
 money, the program must calculate and display the amount plus compound interest after each year*/
 
+int main(){
+    //variables decleared
+    float intRate, amount, totalAmount;
+    int years;
+
+    //prompt the user for input.
+    //get the amount in rands
+    cout << "Enter an amount in rands: ";
+    cin >> amount;
+
+    //get the int rate
+    cout << "Enter the interest rate: ";
+    cin >> intRate;
+
+    //get number of years
+    cout << "Enter amount of years: ";
+    cin >> years;
+
+    //loop through the years while adding interest rate
+    for(int i = 1; i <= years; i++){
+
+        amount += amount * intRate/100;
+
+        cout << "The total amount of compound interest after " << i << " years at a interest rate of " << intRate << " is: " << amount << endl;
+        
+    }
+
+}
