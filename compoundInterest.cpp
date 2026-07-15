7509d142090694c3a81167f083a1fd889f6775d4
Aphiwe Maphanga <maphangaaphiwe7@gmail.com>
2026-07-15 19:59:14 +0200

    created compoundInterest.cpp

---
+++ compoundInterest.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+/*Write a program to calculate the compound interest on an amount of money at a given interest rate, for a
+number of years. After inputting an amount of Rands, an interest rate and a number of years to invest the
+money, the program must calculate and display the amount plus compound interest after each year*/
+
