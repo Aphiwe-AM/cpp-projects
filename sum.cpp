c45950f69b4c5fa4bac590f25963df6e17d04207
Aphiwe Maphanga <maphangaaphiwe7@gmail.com>
2026-07-16 19:22:59 +0200

    completed sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -7,5 +7,27 @@ odd).*/
 
 int main(){
 // variables used 
+    int n; 
+    int oddSum = 0;
+    
 
+    //get number n from the user
+    cout << "please enter any number: ";
+    cin >> n;
+
+    //loop through n numbers
+    for(int i = 1; i <= n; i++){
+        //find all odd numbers
+        if(i % 2 != 0){
+            //display the sum of all odd numbers
+            cout << oddSum << '+' << i << endl;
+
+            //sum the  odd numbers
+            oddSum += i; 
+            
+        }
+        
+    }
+     
+        cout << "The total sum of odd numbers is: "<< oddSum;
 }
