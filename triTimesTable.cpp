b4ada7625ea0fa3f00aad76b75491d4a03bc9596
Aphiwe Maphanga <maphangaaphiwe7@gmail.com>
2026-07-17 15:09:58 +0200

    completed the triangle times table

--- triTimesTable.cpp
+++ triTimesTable.cpp
@@ -5,8 +5,23 @@ int main(){
     //decleared variables
     int length = 9;
 
-  for (int i  = 1; i <= length; i++){
-    continue;
-  }
+    for(int k = 1; k <= length; k++){
+            cout << "  " << k;
+         }
 
+        cout << endl;
+
+    //colums
+  for (int i  = 1; i <= length; i++){
+
+    cout << i << " ";
+    
+    
+    for (int j = 1; j <= i; j++){
+        cout << j * i << " ";
+    
+    }
+    cout << endl;
+  }
+  
 }
