9f06f9c993fa13c705eca2b9b6c91849adef0aeb
Aphiwe Maphanga <maphangaaphiwe7@gmail.com>
2026-07-18 13:58:44 +0200

    created a program to test if values provide can form a valid program.

---
+++ validTri.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/*For every triangle, the sum of (the lengths of) any two sides is greater than the third side. This fact can be
+used to test whether three numbers represent the valid sides of a triangle. write a program that inputs three 
+numbers and calls a function to check whether they represent the sides of a triangle. Hint: You’ll find it useful 
+to write an additional boolean function to test whether the sum of two values is greater than a third.*/
+
+int main(){
+
+}
